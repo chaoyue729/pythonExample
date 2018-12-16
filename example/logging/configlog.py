1109193e0cfdc3e004e1f2c005449ac5c0199a6a
kim jihoon <whitexozu@gmail.com>
2018-12-16 21:07:23 +0900

    .

---
+++ example/logging/configlog.py
@@ -0,0 +1,30 @@
+import logging
+import logging.config
+import json
+import os
+
+
+class LoggerAdapter(logging.LoggerAdapter):
+	def __init__(self, prefix, logger):
+		super(LoggerAdapter, self).__init__(logger, {})
+		self.prefix = prefix
+
+	def process(self, msg, kwargs):
+		return '[%s] %s' % (self.prefix, msg), kwargs
+
+if __name__ == '__main__':
+
+	with open('logging.json', 'rt') as f:
+		config = json.load(f)
+
+	logging.config.dictConfig(config)
+
+	# logger = logging.getLogger()
+	# logger.info("test!!!")
+
+	mylogger = logging.getLogger("my_module")
+	mylogger.info("my test!!!")
+
+	# logger = LoggerAdapter("SAS", logger)
+	# logger.info("test!!!")
+
