b0d1ac13bb72e52faae939532dfa59699f1ae292
kim jihoon <whitexozu@gmail.com>
2018-12-28 17:57:17 +0900

    .

--- example/project/chat_channels/mysite/chat/views.py
+++ example/project/chat_channels/mysite/chat/views.py
@@ -2,22 +2,24 @@ from django.shortcuts import render
 from django.utils.safestring import mark_safe
 from django.http import HttpResponse, HttpResponseRedirect
 from websocket import create_connection
-
+import sys
 import json
 
 def send(request):
-	# wsUrl = 'ws://118.218.215.211:8000/ws/chat/' + roomName + '/'
-	wsUrl = 'ws://118.218.215.211:8000/ws/chat/1111-2222/'
-	ws = create_connection(wsUrl)
-	ws.send(json.dumps({'message': '99999999'}))
-	ws.close()
-
+	raise ValueError
 	return HttpResponse("Hello, world. You're at the polls index.")
 
 def index(request):
-    return render(request, 'chat/index.html', {})
+	raise ValueError
+	# try:
+	# 	raise ValueError
+	# except Exception as e:
+	# 	# print(e)
+	# 	print(sys.exc_info()[0])
+	return render(request, 'chat/index.html', {})
 
 def room(request, room_name):
+
     return render(request, 'chat/room.html', {
         'room_name_json': mark_safe(json.dumps(room_name))
     })
