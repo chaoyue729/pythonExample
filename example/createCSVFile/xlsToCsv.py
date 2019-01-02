4cfa9f2b093ed849c62ce760087822c34f31bfec
kim ji hoon <whitexozu@gmail.com>
2019-01-02 11:09:21 +0900

    .

--- example/createCSVFile/xlsToCsv.py
+++ example/createCSVFile/xlsToCsv.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-data_xls = pd.read_excel('/Users/whitexozu/Downloads/lgscript.xlsx', 'Sheet1', index_col=None)
-data_xls.to_csv('/Users/whitexozu/Downloads/lgscript.xlsx', encoding='utf-8')
+data_xls = pd.read_excel('/Users/whitexozu/Downloads/lgscript.xlsx', 'Sheet', index_col=None)
+data_xls.to_csv('/Users/whitexozu/Downloads/lgscript.csv', encoding='utf-8')
