28a238889fba8d4657833788832988b20a096c2f
kim jihoon <whitexozu@gmail.com>
2019-02-13 17:09:36 +0900

    .

--- example/createCSVFile/xlsToCsv.py
+++ example/createCSVFile/xlsToCsv.py
@@ -1,5 +1,31 @@
 import pandas as pd
 
-data_xls = pd.read_excel('/Users/whitexozu/Downloads/lgscript.xlsx', 'Sheet', index_col=None)
+# data_xls = pd.read_excel('/Users/whitexozu/Downloads/lgscript.xlsx', 'Sheet', index_col=None)
 # data_xls.to_csv('/Users/whitexozu/dev/project/lgu+/data/lgscript.csv', encoding='utf-8', sep='|')
-data_xls.to_csv('/Users/whitexozu/dev/project/LGUplus/data/lgscript.csv', encoding='utf-8', sep='|')
+
+# data_xls = pd.read_excel('/Users/whitexozu/Downloads/temp.xlsx', '31225520181113104730', index_col=None)
+# data_xls.to_csv('/Users/whitexozu/Downloads/temp/31225520181113104730.csv', encoding='utf-8', sep='|', index=False)
+
+# data_xls = pd.read_excel('/Users/whitexozu/Downloads/temp.xlsx', '31220720181108130209', index_col=None)
+# data_xls.to_csv('/Users/whitexozu/Downloads/temp/31220720181108130209.csv', encoding='utf-8', sep='|', index=False)
+
+# data_xls = pd.read_excel('/Users/whitexozu/Downloads/temp.xlsx', '31221820181115164143', index_col=None)
+# data_xls.to_csv('/Users/whitexozu/Downloads/temp/31221820181115164143.csv', encoding='utf-8', sep='|', index=False)
+
+data_xls = pd.read_excel('/Users/whitexozu/Downloads/temp.xlsx', '31220620181105094303', index_col=None)
+data_xls.to_csv('/Users/whitexozu/Downloads/temp/31220620181105094303.csv', encoding='utf-8', sep='|', index=False)
+
+data_xls = pd.read_excel('/Users/whitexozu/Downloads/temp.xlsx', '31230320181116152742', index_col=None)
+data_xls.to_csv('/Users/whitexozu/Downloads/temp/31230320181116152742.csv', encoding='utf-8', sep='|', index=False)
+
+data_xls = pd.read_excel('/Users/whitexozu/Downloads/temp.xlsx', '31221120181212100239', index_col=None)
+data_xls.to_csv('/Users/whitexozu/Downloads/temp/31221120181212100239.csv', encoding='utf-8', sep='|', index=False)
+
+data_xls = pd.read_excel('/Users/whitexozu/Downloads/temp.xlsx', '21270420181212093550', index_col=None)
+data_xls.to_csv('/Users/whitexozu/Downloads/temp/21270420181212093550.csv', encoding='utf-8', sep='|', index=False)
+
+data_xls = pd.read_excel('/Users/whitexozu/Downloads/temp.xlsx', '21290220181101140732', index_col=None)
+data_xls.to_csv('/Users/whitexozu/Downloads/temp/21290220181101140732.csv', encoding='utf-8', sep='|', index=False)
+
+data_xls = pd.read_excel('/Users/whitexozu/Downloads/temp.xlsx', '21270620181034112400', index_col=None)
+data_xls.to_csv('/Users/whitexozu/Downloads/temp/21270620181034112400.csv', encoding='utf-8', sep='|', index=False)
